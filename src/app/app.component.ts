edc55bebdc04d326b823764ff2503fcb02c2d1e4
vignesh Das <vigneshdas3@gmail.com>
2020-10-14 15:26:48 +0530

    Updated Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SupportTools';
+  title = 'SPCSupportTool';
+  dataArr: Array<any>;
+
+  dataTransform(data: HTMLInputElement){
+    const trasformedData = "'"+data.value.replace(/\n/g, "','");
+    data.value = trasformedData;
+  }
+
+  dataTransform1(data: HTMLInputElement){
+    let formatedData = '';
+    this.dataArr = data.value.split('\n');
+    this.dataArr.forEach((value)=>{
+        if (value !== undefined && value !== ''){
+          const index = value.indexOf('/');
+          const length = value.length-1;
+          formatedData +="'"+value.substring(0, index)+"',";
+        }
+    });
+    if (formatedData !== undefined){
+      data.value = formatedData;
+    }
+  }
+
+  addFirst(){
+
+  }
+
+  addLast(){
+
+  }
 }
